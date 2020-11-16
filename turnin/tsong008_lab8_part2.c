c350ef00caa097f9c622b2a09efed75b2deeaa24
Tinghui Song <tinghuigz10@gmail.com>
2020-11-15 22:48:12 -0800

    Added URL's

--- turnin/tsong008_lab8_part2.c
+++ turnin/tsong008_lab8_part2.c
@@ -7,7 +7,7 @@
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
  *
- *	Demo link:
+ *	Demo link: https://drive.google.com/file/d/1kcWdgGjwXdKtYmPMYZUObFNw5xfwstq_/view?usp=sharing
  */
 #include <avr/io.h>
 #ifdef _SIMULATE_
--- turnin/tsong008_lab8_part3.c
+++ turnin/tsong008_lab8_part3.c
@@ -7,7 +7,7 @@
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
  *
- *	Demo link:
+ *	Demo link: https://drive.google.com/file/d/17OMt1YV6hhWHuDzl9FtytlIr7NSdHeJ9/view?usp=sharing
  */
 #include <avr/io.h>
 #ifdef _SIMULATE_
